465c20016cd05dce7a5914b57eb9725a63d345db
Krg72 <60804303+Krg72@users.noreply.github.com>
2020-02-09 13:17:25 +0530

    Add files via upload

---
+++ AI.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int x[8], y[5], z[3];
+	for(int i = 0; i <= 10; i++)
+	{
+		for(int i = 0; i <= 2; i++)
+		{
+			for(int j = 0; j <= 8; j++)
+			{
+				x[j] = 1;
+			}
+			for(int k = 0; k <= 5; k++)
+			{
+				y[k] = x[];
+			}
+		}	
+	}
+	
+}
